0b8c9e0adb62160215ee3c11a4662588265f9a62
Leo Dubert <leodubert@gmail.com>
2017-03-07 17:12:48 +0100

    again a model wth rails, but with the default routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+10.times do
+  Article.new(title: Faker::Book.title, content: Faker::ChuckNorris.fact).save
+
+end
